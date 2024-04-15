6cb873a74e3ef9e99e0dbbaad1c7f414cfa416ff
Vitor Faé <vitorfaeulrich0@gmail.com>
2024-04-15 20:07:49 -0300

    Atividade de ficha cadastral

---
+++ 01042024/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq
+using System.Threading.Tasks
+
+namespace _240401_01.Models
+{
+    Public class product
+        {
+            public 
+
+
+
+
+
+
+        }
+}
